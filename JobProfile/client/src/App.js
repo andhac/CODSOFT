28ec6b41604d1ce416fa2122ef23247906b58f87
andhac <andhac84@yahoo.com>
2024-07-01 16:20:08 +0530

    Make A Search Function

--- JobProfile/client/src/App.js
+++ JobProfile/client/src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Hero from "./components/HeroSection";
+import HomePageLayout from "./layout/HomePage.layout";
+import HomeHoc from "./HOC/Home.HOC";
+import HomePage from "./pages/HomePage";
+import JobResult from "./components/JobResult";
 
 function App() {
   return (
    <>
-    <Navbar/>
-       <Hero/>
+    <HomeHoc exact component={HomePage} path="/" />
+    <HomeHoc exact component={JobResult} path="/search" />
    </>
   );
 }
